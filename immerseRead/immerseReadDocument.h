5c7b1275ef7e7c654243d32031b9ef84c0621ee9
Sohei Okamoto <soheiokamoto@soheio-macbook-air.(none)>
2013-11-13 09:43:53 -0800

    Initial Commit

---
+++ immerseRead/immerseReadDocument.h
@@ -0,0 +1,13 @@
+//
+//  immerseReadDocument.h
+//  immerseRead
+//
+//  Created by Sohei Okamoto on 11/13/13.
+//  Copyright (c) 2013 Sohei Okamoto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface immerseReadDocument : NSDocument
+
+@end
---
+++ immerseRead/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  immerseRead
+//
+//  Created by Sohei Okamoto on 11/13/13.
+//  Copyright (c) 2013 Sohei Okamoto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
